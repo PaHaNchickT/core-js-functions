8e74c076bf8604c0371507007de4f8b101c6d0a1
Pavel <you@example.com>
2024-04-03 14:57:05 +0300

    feat: implement 4 task (03.04.24 14:57)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -74,8 +74,10 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exp) {
+  return (num) => {
+    return num ** exp;
+  };
 }
 
 /**
